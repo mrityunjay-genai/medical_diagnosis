f11d8d5054b97a6755838b2dea441c212de70d94
mrityunjay-genai <mritytunjay.genai@gmail.com>
2025-09-15 15:38:22 +0530

    main created

--- langserve_backend/main.py
+++ langserve_backend/main.py
@@ -1,3 +1,8 @@
 from fastapi import FastAPI
 from langserve import add_routes
 from diagnostic_graph import build_graph
+
+app = FastAPI()
+graph = build_graph()
+
+add_routes(app, graph, path="/diagnose")
